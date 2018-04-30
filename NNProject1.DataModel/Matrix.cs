bec92ff1c3d024bfb2b3efeb49ed14bf8051b127
Dell-Note\Иван <ivan070297@gmail.com>
2018-04-30 08:54:10 +0300

    Add project structure

---
+++ NNProject1.DataModel/Matrix.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NNProject1.DataModel
+{
+    /// <summary>
+    /// Represents integer matrix A of size N x M 
+    /// </summary>
+    public class Matrix
+    {
+        
+    }
+}
