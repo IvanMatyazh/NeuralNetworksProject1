e5be741f901dbd95e24080ebee6f2754be9e1a03
Dell-Note\Иван <ivan070297@gmail.com>
2018-05-01 22:06:31 +0300

    Add matrix class implementation

--- NNProject1.DataModel/Matrix.cs
+++ NNProject1.DataModel/Matrix.cs
@@ -7,10 +7,278 @@ using System.Threading.Tasks;
 namespace NNProject1.DataModel
 {
     /// <summary>
-    /// Represents integer matrix A of size N x M 
+    /// Represents integer matrix A of size M x N 
     /// </summary>
     public class Matrix
     {
-        
+        /// <summary>
+        /// Elements of the matrix
+        /// </summary>
+        public int[,] Values { get; protected set; }
+
+        /// <summary>
+        /// Number of rows in the matrix
+        /// </summary>
+        public int NRows { get; protected set; }
+
+        /// <summary>
+        /// Number of columns in the matrix
+        /// </summary>
+        public int NColumns { get; protected set; }
+
+        protected Matrix() {}
+
+        /// <summary>
+        /// Constructs square Identity matrix of size n
+        /// </summary>
+        /// <param name="n">Number of rows</param>
+        public Matrix(int n)
+        {
+            NRows = NColumns = n;
+            Values = new int[n, n];
+            for (int i = 0; i < n; i++)
+                Values[i, i] = 1;
+        }
+
+        /// <summary>
+        /// Constructs Zero matrix of size nrows by ncolumns
+        /// </summary>
+        /// <param name="nrows">Number of rows</param>
+        /// <param name="ncolumns">Number of columns</param>
+        public Matrix(int nrows, int ncolumns)
+        {
+            Values = new int[nrows, ncolumns];
+            NRows = nrows;
+            NColumns = ncolumns;
+        }
+
+        /// <summary>
+        /// Constructs matrix from 2D array
+        /// </summary>
+        /// <param name="values">Elements of the matrix</param>
+        public Matrix(int[,] values)
+        {
+            NRows = values.GetLength(0);
+            NColumns = values.GetLength(1);
+            Values = new int[NRows, NColumns];
+            for (int i = 0; i < NRows; i++)
+                for (int j = 0; j < NColumns; j++)
+                    Values[i, j] = values[i, j];
+        }
+
+        /// <summary>
+        /// Constructs Matrix from list of lists 
+        /// </summary>
+        /// <param name="values">Elements of the matrix</param>
+        /// <param name="nrows">Maximal number of rows</param>
+        /// <param name="ncolumns">Maximal number of columns</param>
+        public Matrix(List<List<int>> values, int nrows, int ncolumns)
+            : this(nrows, ncolumns)
+        {
+            for (int i = 0; i < nrows; i++)
+            {
+                for (int j = 0; j < ncolumns; j++)
+                {
+                    if (values.Count > i && values[i].Count > j)
+                        Values[i, j] = values[i][j];
+                    else
+                        Values[i, j] = 0;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Constructs matrix from another matrix
+        /// </summary>
+        /// <param name="m">Input matrix</param>
+        /// <param name="transpose">Flag indicates whether to transpose the matrix m</param>
+        public Matrix(Matrix m, bool transpose)
+        {
+            NRows = transpose ? m.NColumns : m.NRows;
+            NColumns = transpose ? m.NRows : m.NColumns;
+            Values = new int[NRows, NColumns];
+            for (int i = 0; i < NRows; i++)
+                for (int j = 0; j < NColumns; j++)
+                    Values[i, j] = transpose ? m.Values[j, i] : m.Values[i, j];
+        }
+
+        public int this[int i, int j]
+        {
+            get
+            {
+                if (i < NRows && j < NColumns)
+                    return Values[i, j];
+                else
+                    throw new ArgumentOutOfRangeException("Indices of the matrix are out of range");
+            }
+            set
+            {
+                if (i < NRows && j < NColumns)
+                    Values[i, j] = value;
+                else
+                    throw new ArgumentOutOfRangeException("Indices of the matrix are out of range");
+            }
+        }
+
+        /// <summary>
+        /// Scalar multiplication of matrix
+        /// </summary>
+        /// <param name="scale">Scalar</param>
+        /// <param name="m">Matrix</param>
+        /// <returns></returns>
+        public static Matrix operator *(int scale, Matrix m)
+        {
+            Matrix res = new Matrix(m.NRows, m.NColumns);
+            for (int i = 0; i < res.NRows; i++)
+                for (int j = 0; j < res.NColumns; j++)
+                    res[i, j] = scale * m[i, j];
+            return res;
+        }
+
+        /// <summary>
+        /// Substraction of matrices
+        /// </summary>
+        /// <param name="m1">First matrix</param>
+        /// <param name="m2">Second matrix</param>
+        /// <returns></returns>
+        public static Matrix operator -(Matrix m1, Matrix m2)
+        {
+            if ((m1.NRows == m2.NRows) && (m1.NColumns == m2.NColumns))
+            {
+                Matrix res = new Matrix(m1.NRows, m1.NColumns);
+                for (int i = 0; i < res.NRows; i++)
+                    for (int j = 0; j < res.NColumns; j++)
+                        res[i, j] = m1[i, j] - m2[i, j];
+                return res;
+            }
+            else
+            {
+                throw new ArgumentException("Matrices should have the same size");
+            }
+        }
+
+        /// <summary>
+        /// Sum of matrices
+        /// </summary>
+        /// <param name="m1">First matrix</param>
+        /// <param name="m2">Second matrix</param>
+        /// <returns></returns>
+        public static Matrix operator +(Matrix m1, Matrix m2)
+        {
+            if ((m1.NRows == m2.NRows) && (m1.NColumns == m2.NColumns))
+            {
+                Matrix res = new Matrix(m1.NRows, m1.NColumns);
+                for (int i = 0; i < res.NRows; i++)
+                    for (int j = 0; j < res.NColumns; j++)
+                        res[i, j] = m1[i, j] + m2[i, j];
+                return res;
+            }
+            else
+            {
+                throw new ArgumentException("Matrices should have the same size");
+            }
+        }
+
+        /// <summary>
+        /// Matrix by vector multipliplication
+        /// </summary>
+        /// <param name="m">Matrix</param>
+        /// <param name="vect">Vector</param>
+        /// <returns></returns>
+        public static List<int> operator *(Matrix m, List<int> vect)
+        {
+            if (vect.Count == m.NColumns)
+            {
+                List<int> res = new List<int>();
+                for (int i = 0; i < m.NRows; i++)
+                {
+                    int temp = 0;
+                    for (int j = 0; j < m.NColumns; j++)
+                    {
+                        temp += m[i, j] * vect[j];
+                    }
+                    res.Add(temp);
+                }
+                return res;
+            }
+            else
+            {
+                throw new ArgumentException("The length of vector should be the same as number of columns in matrix");
+            }
+        }
+
+        /// <summary>
+        /// Matrix by vector multiplication
+        /// </summary>
+        /// <param name="m">Matrix</param>
+        /// <param name="vect">Vector</param>
+        /// <returns></returns>
+        public static int[] operator *(Matrix m, int[] vect)
+        {
+            if (vect.Length == m.NColumns)
+            {
+                int[] res = new int[m.NRows];
+                for (int i = 0; i < m.NRows; i++)
+                {
+                    int temp = 0;
+                    for (int j = 0; j < m.NColumns; j++)
+                    {
+                        temp += m[i, j] * vect[j];
+                    }
+                    res[i] = temp;
+                }
+                return res;
+            }
+            else
+            {
+                throw new ArgumentException("The length of vector should be the same as number of columns in matrix");
+            }
+        }
+
+        /// <summary>
+        /// Matrix multiplication
+        /// </summary>
+        /// <param name="m1">First matrix</param>
+        /// <param name="m2">Second matrix</param>
+        /// <returns></returns>
+        public static Matrix operator *(Matrix m1, Matrix m2)
+        {
+            if (m1.NColumns == m2.NRows)
+            {
+                Matrix res = new Matrix(m1.NRows, m2.NColumns);
+                for (int i = 0; i < res.NRows; i++)
+                {
+                    for (int j = 0; j < res.NColumns; j++)
+                    {
+                        int temp = 0;
+                        for (int p = 0; p < m1.NColumns; p++)
+                        {
+                            temp += m1[i, p] * m2[p, j];
+                        }
+                        res[i, j] = temp;
+                    }
+                }
+                return res;
+            }
+            else
+            {
+                throw new ArgumentException("# of columns of matrix m1 and # of rows of matrix m2 should be the same");
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < NRows; i++)
+            {
+                for (int j = 0; j < NColumns; j++)
+                {
+                    builder.Append(Values[i, j]);
+                    builder.Append(" ");
+                }
+                builder.Append("\n");
+            }
+            return builder.ToString();
+        }
     }
 }
--- NNProject1.Tests/MatrixTests.cs
+++ NNProject1.Tests/MatrixTests.cs
@@ -1,14 +1,264 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NNProject1.DataModel;
+using System.Collections.Generic;
 
 namespace NNProject1.Tests
 {
     [TestClass]
     public class MatrixTests
     {
+        int n = 3;
+
+        int[,] I = { {1, 0, 0},
+                     {0, 1, 0},
+                     {0, 0, 1} };
+
+        int[,] arr = { { 1, 2 },
+                       { 3, 4 },
+                       { 5, 6 } };
+
+        int[] vect = { 1, 1 };
+
+        List<int> vect2 = new List<int>() { 1, 1 };
+
+        int[] multResultVector = { 3, 7, 11 };
+
+        int[,] arr2 = { { 2, 3 },
+                        { 4, 5 },
+                        { 6, 7 } };
+
+        int[,] arr3 = { { 1, 2, 3},
+                        { 4, 5, 6} };
+
+        int[,] multResultMatrix = { { 9, 12, 15 },
+                                    { 19, 26, 33 },
+                                    { 29, 40, 51 } };
+
         [TestMethod]
-        public void TestMethod1()
+        public void ConstructIdentityMatrix()
         {
+            Matrix A = new Matrix(n);
+            Assert.AreEqual(A.NRows, n);
+            Assert.AreEqual(A.NColumns, n);
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    Assert.AreEqual(A[i, j], I[i, j]); 
+        }
+
+        [TestMethod]
+        public void ConstructMatrixFrom2DArray()
+        {
+            Matrix A = new Matrix(arr);
+            Assert.AreEqual(A.NRows, 3);
+            Assert.AreEqual(A.NColumns, 2);
+            for (int i = 0; i < A.NRows; i++)
+                for (int j = 0; j < A.NColumns; j++)
+                    Assert.AreEqual(A[i, j], arr[i, j]);
+        }
+
+        [TestMethod]
+        public void ConstructRectangularMatrix()
+        {
+            Matrix A = new Matrix(3, 4);
+            Assert.AreEqual(A.NRows, 3);
+            Assert.AreEqual(A.NColumns, 4);
+            for (int i = 0; i < A.NRows; i++)
+                for (int j = 0; j < A.NColumns; j++)
+                    Assert.AreEqual(A[i, j], 0);
+        }
+
+        [TestMethod]
+        public void ConstructMatrixFromListOfLists()
+        {
+            List<List<int>> list = new List<List<int>>();
+            for (int j = 0; j < 4; j++)
+            {
+                List<int> values = new List<int>();
+                for (int i = 0; i < 3; i++)
+                    values.Add(i);
+                list.Add(values);
+            }
+            Matrix A = new Matrix(list, 4, 3);
+            Assert.AreEqual(A.NRows, 4);
+            Assert.AreEqual(A.NColumns, 3);
+            for (int i = 0; i < A.NRows; i++)
+                for (int j = 0; j < A.NColumns; j++)
+                    Assert.AreEqual(A[i, j], list[i][j]);
+        }
+
+        [TestMethod]
+        public void ConstructMatrixFromAnotherMatrix()
+        {
+            Matrix A = new Matrix(arr);
+            Matrix Acopy = new Matrix(A, false);
+            Assert.AreEqual(Acopy.NRows, A.NRows);
+            Assert.AreEqual(Acopy.NColumns, A.NColumns);
+            for (int i = 0; i < A.NRows; i++)
+                for (int j = 0; j < A.NColumns; j++)
+                    Assert.AreEqual(Acopy[i, j], A[i, j]);
+        }
+
+        [TestMethod]
+        public void ConstructMatrixFromAnotherMatrixTransposed()
+        {
+            Matrix A = new Matrix(arr);
+            Matrix Acopy = new Matrix(A, true);
+            Assert.AreEqual(Acopy.NRows, A.NColumns);
+            Assert.AreEqual(Acopy.NColumns, A.NRows);
+            for (int i = 0; i < Acopy.NRows; i++)
+                for (int j = 0; j < Acopy.NColumns; j++)
+                    Assert.AreEqual(Acopy[i, j], A[j, i]);
+        }
+
+        [TestMethod]
+        public void ConstructMatrixFromJaggedListOfLists()
+        {
+            List<List<int>> list = new List<List<int>>();
+            for (int i = 0; i < 3; i++)
+            {
+                List<int> values = new List<int>();
+                for (int j = 0; j < i + 1; j++)
+                    values.Add(i + 1);
+                list.Add(values);
+            }
+            Matrix A = new Matrix(list, 3, 3);
+            Assert.AreEqual(A.NRows, 3);
+            Assert.AreEqual(A.NColumns, 3);
+            for (int i = 0; i < A.NRows; i++)
+                for (int j = 0; j < A.NColumns; j++)
+                    Assert.AreEqual(A[i, j], j < i + 1 ? list[i][j] : 0);
+        }
+
+        [TestMethod]
+        public void ScaleMatrix()
+        {
+            Matrix A = new Matrix(arr);
+            Matrix res = 5 * A;
+            Assert.AreEqual(res.NRows, A.NRows);
+            Assert.AreEqual(res.NColumns, A.NColumns);
+            for (int i = 0; i < res.NRows; i++)
+                for (int j = 0; j < res.NColumns; j++)
+                    Assert.AreEqual(res[i, j], arr[i, j] * 5);
+        }
+
+        [TestMethod]
+        public void SumMatrices()
+        {
+            Matrix A = new Matrix(arr);
+            Matrix B = new Matrix(arr2);
+            Matrix res = A + B;
+            Assert.AreEqual(res.NRows, A.NRows);
+            Assert.AreEqual(res.NColumns, A.NColumns);
+            for (int i = 0; i < res.NRows; i++)
+                for (int j = 0; j < res.NColumns; j++)
+                    Assert.AreEqual(res[i, j], arr[i, j] + arr2[i, j]);
+        }
+
+        [TestMethod]
+        public void SubtractMatrices()
+        {
+            Matrix A = new Matrix(arr);
+            Matrix B = new Matrix(arr2);
+            Matrix res = A - B;
+            Assert.AreEqual(res.NRows, A.NRows);
+            Assert.AreEqual(res.NColumns, A.NColumns);
+            for (int i = 0; i < res.NRows; i++)
+                for (int j = 0; j < res.NColumns; j++)
+                    Assert.AreEqual(res[i, j], arr[i, j] - arr2[i, j]);
+        }
+
+        [TestMethod]
+        public void MultiplyMatrixByVector()
+        {
+            Matrix A = new Matrix(arr);
+            List<int> res = A * vect2;
+            Assert.AreEqual(res.Count, multResultVector.Length);
+            for (int i = 0; i < vect2.Count; i++)
+                Assert.AreEqual(res[i], multResultVector[i]);
+        }
+
+        [TestMethod]
+        public void MultiplyMatrixByArray()
+        {
+            Matrix A = new Matrix(arr);
+            int[] res = A * vect;
+            Assert.AreEqual(res.Length, multResultVector.Length);
+            for (int i = 0; i < vect.Length; i++)
+                Assert.AreEqual(res[i], multResultVector[i]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void MultiplyMatrixByVectorOfNotEqualSize()
+        {
+            Matrix A = new Matrix(arr);
+            List<int> testVect = new List<int> { 1, 1, 1 };
+            List<int> res = A * testVect;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void MultiplyMatrixByArrayOfNotEqualSize()
+        {
+            Matrix A = new Matrix(arr);
+            int[] testVect = new int[3];
+            int[] res = A * testVect;
+        }
+
+        [TestMethod]
+        public void MultiplyMatrixByMatrix()
+        {
+            Matrix A = new Matrix(arr);
+            Matrix B = new Matrix(arr3);
+            Matrix res = A * B;
+            Assert.AreEqual(res.NRows, A.NRows);
+            Assert.AreEqual(res.NColumns, B.NColumns);
+            for (int i = 0; i < res.NRows; i++)
+                for (int j = 0; j < res.NColumns; j++)
+                    Assert.AreEqual(res[i, j], multResultMatrix[i, j]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void MultiplyMatrixByMatrixOfDifferentSizes()
+        {
+            Matrix A = new Matrix(arr);
+            Matrix B = new Matrix(arr2);
+            Matrix res = A * B;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SubtractMatricesWithNotEqualSize()
+        {
+            Matrix A = new Matrix(arr);
+            Matrix B = new Matrix(arr3);
+            Matrix res = A - B;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SumMatricesWithNotEqualSize()
+        {
+            Matrix A = new Matrix(arr);
+            Matrix B = new Matrix(arr3);
+            Matrix res = A + B;
+        }
+
+        [TestMethod]
+        public void RetriveValueOfMatrix()
+        {
+            Matrix A = new Matrix(arr);
+            Assert.AreEqual(A[1, 1], 4);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void RetriveValueOutOfRangeOfMatrix()
+        {
+            Matrix A = new Matrix(arr);
+            int value = A[5, 5];
         }
     }
 }
