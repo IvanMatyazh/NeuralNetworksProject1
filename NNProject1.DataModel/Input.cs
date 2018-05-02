059ba04f99f28b6e712086ab8665bff841ce4f5e
fardeenmohd <fardeenmohdpl@gmail.com>
2018-05-02 19:36:13 +0200

    Input class addded

---
+++ NNProject1.DataModel/Input.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NNProject1.DataModel;
+using System.IO;
+
+namespace NNProject1.DataModel
+{
+    public class Input
+    {
+        public List<List<int>> Values { get; protected set; }
+        public int vectorSize{ get; protected set; }
+        public int numberOfVectors { get; protected set; }
+        protected Input() { }
+        
+        public Input(Stream fileStream)
+        {
+            Values = new List<List<int>>();
+            String line;
+            StreamReader reader = new StreamReader(fileStream);
+            char[] charSeparators = new char[] { ',', ' ' };
+            int currentVector = 0;
+            using (fileStream)
+            {
+                line = reader.ReadLine();// Reading the header with the size of each vector
+                vectorSize = Int32.Parse(line);
+                while((line = reader.ReadLine()) != null )
+                {
+                    string[] lineSplit = line.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+                    if (lineSplit.Length == vectorSize)
+                    {
+                        List<int> tempVector = new List<int>(vectorSize);
+                        foreach (var elem in lineSplit)
+                        {
+                            tempVector.Add(Int32.Parse(elem));
+                        }
+                        Values.Add(tempVector);
+                        currentVector++;
+                    }
+                    else
+                    {
+                        fileStream.Close();
+                        throw new ArgumentException("The vector at line: " + (currentVector + 1) + " should be of length: " + vectorSize + " instead of " + lineSplit.Length);
+
+                    }
+                }
+            }
+            numberOfVectors = currentVector;
+            fileStream.Close();
+
+        }
+
+    }
+}
--- NNProject1.GUI/MainWindow.xaml.cs
+++ NNProject1.GUI/MainWindow.xaml.cs
@@ -23,94 +23,53 @@ namespace NNProject1.GUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<List<int>> input;
-        int vectorSize;
-        int numberOfVectors;
-        bool isHeader;
+        Input input;
         AssociativeMemory associativeMemory;
         List<int> testVector;
-        char[] charSeparators = new char[] { ',', ' ' };
-
-
+        
         public MainWindow()
         {
             InitializeComponent();
-            
-            isHeader = true;
             AppendToConsole("Welcome to our Neural Networks Project");
-
-
         }
 
         private void OpenFile_Click(object sender, RoutedEventArgs e)
         {
-            input = new List<List<int>>();
-            vectorSize = 0;
-            numberOfVectors = 0;
-            String line;
-            StreamReader myStream = null;
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
             openFileDialog.RestoreDirectory = true;
-            int currentVector = 0;
             if (openFileDialog.ShowDialog() == true)
             {
                 try
                 {
-                    if ((myStream = new StreamReader(openFileDialog.OpenFile())) != null)
-                    {
-                        using (myStream)
-                        {
-                            line = myStream.ReadLine();// Reading the header with the size of each vector
-                            vectorSize = Int32.Parse(line);
-                            isHeader = false;
-                            
-                            while ((((line = myStream.ReadLine()) != null)) && isHeader == false)
-                            {
-
-                                string[] lineSplit = line.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
-                                if (lineSplit.Length == vectorSize)
-                                {
-                                    List<int> tempVector = new List<int>(vectorSize);
-                                    foreach(var elem in lineSplit)
-                                    {
-                                        tempVector.Add(Int32.Parse(elem));
-                                    }
-                                    input.Add(tempVector);
-                                    AppendToConsole("Vector " + currentVector);
-                                    AppendListToConsole(tempVector);
-                                    currentVector++;
-                                }
-                                else
-                                {
-                                    MessageBox.Show("The vector at line: " + (currentVector + 1) + " should be of length: " + vectorSize + " instead of " + lineSplit.Length);
-                                    myStream.Close();
-                                    currentVector = 0;
-                                }   
-                            }
-                        }
-                    }
-                    numberOfVectors = currentVector;
-                    myStream.Close();
-                    CreateAssociativeMemoryButton.IsEnabled = true;
-                    AppendToConsole(numberOfVectors + " vectors parsed with " + vectorSize + " elements each");
-                    
-                    
+                    input = new Input(openFileDialog.OpenFile());
                 }
-                catch (Exception ex)
+                catch(Exception ex)
                 {
-                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                    MessageBox.Show("Error creating input class" + ex.Message);
+                }
+                finally
+                {
+                    if(input != null)
+                    {
+                        int index = 0;
+                        foreach (List<int> l in input.Values)
+                        {
+                            AppendToConsole("Vector " + index);
+                            AppendListToConsole(l);
+                            index++;
+                        }
+                        CreateAssociativeMemoryButton.IsEnabled = true;
+                    }
                 }
             }
-
         }
 
         private void CreateAssociativeMemoryButton_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                associativeMemory = new AssociativeMemory(input, numberOfVectors, vectorSize);
-
+                associativeMemory = new AssociativeMemory(input.Values, input.numberOfVectors, input.vectorSize);
             }
             catch(Exception ex)
             {
@@ -118,16 +77,52 @@ namespace NNProject1.GUI
             }
             finally
             {
-                MessageBox.Show("Associative memory created");
-                InputTextBox.IsEnabled = true;
-                TestVectorButton.IsEnabled = true;
-
-                AppendToConsole("Hopfield Matrix");
-                AppendToConsole(associativeMemory.T.ToString());
-                AppendToConsole("Now you can input a test vector of length "+ vectorSize);
-                InputTextBox.Clear();
+                if(associativeMemory != null)
+                {
+                    AppendToConsole("Associative memory created");
+                    InputTextBox.IsEnabled = true;
+                    TestVectorButton.IsEnabled = true;
+                    AppendToConsole("Hopfield Matrix");
+                    AppendToConsole(associativeMemory.T.ToString());
+                    AppendToConsole("Now you can input a test vector of length " + input.vectorSize);
+                    InputTextBox.Clear();
+                }
+                
             }
         }
+
+        private void TestVectorButton_Click(object sender, RoutedEventArgs e)
+        {
+            testVector = new List<int>();
+            string line = InputTextBox.Text;
+            char[] charSeparators = new char[] { ',', ' ' };
+            if (line != null)
+            {
+                string[] lineSplit = line.Split(charSeparators,StringSplitOptions.RemoveEmptyEntries);
+                if (lineSplit.Length == input.vectorSize)
+                {
+                    foreach(var v in lineSplit)
+                    {
+                        testVector.Add(Int32.Parse(v));
+                    }
+                    AppendToConsole("Test Vector");
+                    AppendListToConsole(testVector);
+                    var result = associativeMemory.Test(testVector);
+                    AppendToConsole("Result after testing");
+                    AppendListToConsole(result.Result);
+                }
+                else
+                {
+                    MessageBox.Show("Your vector should be of length "+input.vectorSize+" instead of "+ lineSplit.Length);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please try with a valid vector again");
+            }
+
+        }
+
         private void AppendFreeLineToConsole()
         {
             textBox.AppendText("\n");
@@ -152,40 +147,6 @@ namespace NNProject1.GUI
             text.Append("]");
 
             AppendToConsole(text.ToString());
-
-        }
-
-        private void TestVectorButton_Click(object sender, RoutedEventArgs e)
-        {
-            testVector = new List<int>();
-            string line = InputTextBox.Text;
-            
-            if (line != null)
-            {
-                string[] lineSplit = line.Split(charSeparators,StringSplitOptions.RemoveEmptyEntries);
-                if (lineSplit.Length == vectorSize)
-                {
-                    foreach(var v in lineSplit)
-                    {
-                        testVector.Add(Int32.Parse(v));
-                    }
-                    AppendToConsole("Test Vector");
-                    AppendListToConsole(testVector);
-
-                    var result = associativeMemory.Test(testVector);
-                    AppendToConsole("Result after testing");
-                    AppendListToConsole(result.Result);
-                    
-                }
-                else
-                {
-                    MessageBox.Show("Your vector should be of length "+vectorSize+" instead of "+ lineSplit.Length);
-                }
-            }
-            else
-            {
-                MessageBox.Show("Please try with a valid vector again");
-            }
 
         }
 
