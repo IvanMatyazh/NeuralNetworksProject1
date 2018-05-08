ecca559bfc8d4885391236ccb319ba98ad686478
IvanMatyazh <ivan070297@gmail.com>
2018-05-08 17:48:30 +0200

    Add basic bindings to the input matrix window

--- NNProject1.GUI/UI.Windows/InputMatrixWindow.xaml.cs
+++ NNProject1.GUI/UI.Windows/InputMatrixWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,11 +18,64 @@ namespace NNProject1.GUI.UI.Windows
     /// <summary>
     /// Interaction logic for InputMatrixWindow.xaml
     /// </summary>
-    public partial class InputMatrixWindow : Window
+    public partial class InputMatrixWindow : Window, INotifyPropertyChanged
     {
+        private int _n = 4;
+        private int _m = 4;
+        private List<int> _elements = new List<int>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int N
+        {
+            get
+            {
+                return _n;
+            }
+            set
+            {
+                _n = value;
+                OnPropertyChanged(nameof(N));
+            }
+        }
+        public int M
+        {
+            get
+            {
+                return _m;
+            }
+            set
+            {
+                _m = value;
+                OnPropertyChanged(nameof(M));
+            }
+        }
+        public List<int> Elements
+        {
+            get
+            {
+                return _elements;
+            }
+            set
+            {
+                _elements = value;
+                OnPropertyChanged(nameof(Elements));
+            }
+        }
+
         public InputMatrixWindow()
         {
-            InitializeComponent();
+            for (int i = 0; i < N*M; i++)
+            {
+                Elements.Add(i);
+            }      
+            DataContext = this;
+            InitializeComponent();         
+        }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
